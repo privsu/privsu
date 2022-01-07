3d56f3ccac81c926f801983f5329888d4d07b2f9
privsu <96994821+privsu@users.noreply.github.com>
2022-01-07 03:01:55 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "1afd334cb5284f8888cb871b65f60b1b"}'
+<!-- Cloudflare Web Analytics -->
+
+<script async>
+
+  (function() {
+
+    var el = document.createElement('script');
+
+    el.setAttribute('data-cf-beacon', '{"token": "1afd334cb5284f8888cb871b65f60b1b"}'); //<<<---Insert token here
+
+    el.src='https://static.cloudflareinsights.com/beacon.min.js'
+
+    el.setAttribute('async', '')
+
+    document.body.appendChild(el);
+
+  })();
+
+</script>
+
+<!-- End Cloudflare Web Analytics -->
