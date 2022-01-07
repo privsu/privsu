c1a84034e217c8fc8973128526efca2d7618db0b
privsu <96994821+privsu@users.noreply.github.com>
2022-01-07 04:05:06 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-function includeJs(jsFilePath) {
-    var js = document.createElement("script");
-
-    js.type = "text/javascript";
-    js.src = jsFilePath;
-
-    document.body.appendChild(js);
-}
-
-includeJs("https://static.cloudflareinsights.com/beacon.min.js?token=1afd334cb5284f8888cb871b65f60b1b");
+document.writeln("<script type='text/javascript' src='https://static.cloudflareinsights.com/beacon.min.js?token=1afd334cb5284f8888cb871b65f60b1b'></script>");
